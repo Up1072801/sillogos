47478aa224aa17cd188a01cdb3e5699d6ac0030f
Up1072801 <125863183+Up1072801@users.noreply.github.com>
2025-05-20 15:59:55 +0300

    g

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use('/eidi-sindromis', eidiSindromisRouter);
 app.use(adminRouter);
 
 // Make sure server listens on all interfaces
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
